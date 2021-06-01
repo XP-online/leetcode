60e97dd03d274e825b4ab349b81f39d83a3b471d
pan <panhongwei@tal.com>
2021-06-01 12:51:51 +0800

    完成Q3

--- Q3--longest-substring-without-repeating-characters/code.cpp
+++ Q3--longest-substring-without-repeating-characters/code.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
+    int lengthOfLongestSubstring(string s)
+    {
+        map<char, int> window;
+        int left = 0, right = 0;
+        int res=0;
+        while(right < s.size()){
+            //window.
+            char c = s[right];
 
-    int lengthOfLongestSubstring(string s) {
-
+            window[c]++;
+            while(window[c] > 1){
+                char strleft = s[left];
+                window[strleft]--;
+                left++;
+            }
+            res = max(res, right-left+1);
+            right++;
+        }
+        return res;
     }
 };
 
-int main(){
+int main()
+{
     Solution s;
-    int res = s.lengthOfLongestSubstring("");
-    cout<<res<<endl;
+    int res = s.lengthOfLongestSubstring("pwwkew");
+    cout << res << endl;
     return 0;
 }
